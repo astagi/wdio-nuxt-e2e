348fa272a595f7689d2277c52f73a279ebf689ec
Andrea Stagi <stagi.andrea@gmail.com>
2020-07-14 15:08:26 +0200

    Build and serve Nuxt app in Webdriver hooks (#1)
    * Build and serve Nuxt app in Webdriver hooks
    
    * Build app before tests only under CI
    
    * Add missing $ in CI
    
    * Test refactoring

--- test/e2e/pageObjects/main.page.js
+++ test/e2e/pageObjects/main.page.js
@@ -1,6 +1,7 @@
-class Page {
+class TodoListPage {
   get itemInput () { return $('input[type=text]') }
   get listItems () { return $$('.todolist__item') }
+  get listSize () { return this.listItems.length }
 
   open (path = '/') {
     browser.url(path)
@@ -15,6 +16,14 @@ class Page {
       this.addListItem(item)
     })
   }
+
+  isItemCompletedAt (position) {
+    return this.listItems[position].getAttribute('class').includes('done')
+  }
+
+  completeItemAt (position) {
+    this.listItems[position].click()
+  }
 }
 
-module.exports = new Page()
+module.exports = new TodoListPage()
