51abb6284459782b04958bd78f55ebb0a8159765
Andrea Stagi <stagi.andrea@gmail.com>
2020-07-14 15:07:50 +0200

    Test refactoring

--- test/e2e/specs/index.spec.js
+++ test/e2e/specs/index.spec.js
@@ -1,21 +1,21 @@
-const Page = require('../pageObjects/main.page')
+const TodoListPage = require('../pageObjects/main.page')
 
-describe('Example test', () => {
+describe('Todolist', () => {
   it('should open correct app', () => {
-    Page.open()
+    TodoListPage.open()
     expect(browser).toHaveTitle('todolist')
   })
   it('should add items to the list correctly', () => {
-    Page.open()
-    Page.addListItems(['Milk', 'Apples', '1 Banana'])
-    expect(Page.listItems.length).toEqual(3)
+    TodoListPage.open()
+    TodoListPage.addListItems(['Milk', 'Apples', '1 Banana'])
+    expect(TodoListPage.listSize).toEqual(3)
   })
-  it('should remove items from list correctly', () => {
-    Page.open()
-    Page.addListItems(['Milk', 'Apples', '1 Banana', 'Meat'])
-    expect(Page.listItems.length).toEqual(4)
-    expect(Page.listItems[2].getAttribute('class')).not.toContain('done')
-    Page.listItems[2].click()
-    expect(Page.listItems[2].getAttribute('class')).toContain('done')
+  it('should complete items correctly', () => {
+    TodoListPage.open()
+    TodoListPage.addListItems(['Milk', 'Apples', '1 Banana', 'Meat'])
+    expect(TodoListPage.listSize).toEqual(4)
+    expect(TodoListPage.isItemCompletedAt(2)).toBeFalsy()
+    TodoListPage.completeItemAt(2)
+    expect(TodoListPage.isItemCompletedAt(2)).toBeTruthy()
   })
 })
